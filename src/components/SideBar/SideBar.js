37d9ab11f6014f74cd391083228ce18ddf1dcaa6
Marin M-U <marin.umegane.m@gmail.com>
2024-01-15 19:26:39 -0500

    create sidebar and main text

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,35 +5,30 @@ import linkedin from "../../images/linkedin-logo-circular-15919.svg";
 function Sidebar() {
   return (
     <nav className="sidebar">
-      <div className="sidebar__info">
-        <h4 className="sidebar__social-heading">Reach me via:</h4>
-        <ul className="sidebar__list">
-          <li className="sidebar__list-item">
-            <a
-              href="mailto:marin.umegane.m@gmail.com"
-              className="sidebar__social-link"
-            >
-              <img
-                src={mail}
-                alt="Email icon"
-                className="sidebar__social-icon"
-              />
-            </a>
-          </li>
-          <li className="sidebar__list-item">
-            <a
-              href="https://www.linkedin.com/in/marin-umegane-24b337140/"
-              className="sidebar__social-link"
-            >
-              <img
-                src={linkedin}
-                alt="Linkedin icon"
-                className="sidebar__social-icon"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="sidebar__list">
+        <li className="sidebar__list-item">
+          <a
+            href="mailto:marin.umegane.m@gmail.com"
+            className="sidebar__social-link"
+          >
+            <img src={mail} alt="Email icon" className="sidebar__social-icon" />
+            Email
+          </a>
+        </li>
+        <li className="sidebar__list-item sidebar__list-item_linkedin">
+          <a
+            href="https://www.linkedin.com/in/marin-umegane-24b337140/"
+            className="sidebar__social-link"
+          >
+            <img
+              src={linkedin}
+              alt="Linkedin icon"
+              className="sidebar__social-icon"
+            />
+            LinkedIn
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
