56f686fd44238b93f8263bdf3e2e5543909511f6
Marin M-U <marin.umegane.m@gmail.com>
2024-02-25 16:14:11 -0500

    create temp build ver

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -49,13 +49,61 @@ function Project({ onSelectedCard }) {
             The projects I worked/have been working on:
           </h2>
           <ul className="project__list">
-            {projectList.map((item) => {
+            {/* {projectList.map((item) => {
               <ProjectCard
                 key={item._id}
                 item={item}
                 onSelectedCard={onSelectedCard}
               />;
-            })}
+            })} */}
+            <li className="project__list-item">
+              <a
+                className="project__list-item_elem_link"
+                href="https://github.com/MarinU-M/project-randompattern-frontend"
+              >
+                MadewithLove
+              </a>
+              <p className="project__list-item_elem_description">
+                <span>Tools: React, CSS</span>
+                <span>For: BootCamp final project (frontend)</span>
+              </p>
+            </li>
+            <li className="project__list-item">
+              <a
+                className="project__list-item_elem_link"
+                href="https://marinu-m.github.io/se_project_react/"
+              >
+                WTWR
+              </a>
+              <p className="project__list-item_elem_description">
+                <span>Tools: JavaScript, React, Express, MongoDB</span>
+                <span>For: BootCamp project</span>
+              </p>
+            </li>
+            <li className="project__list-item">
+              <a
+                className="project__list-item_elem_link"
+                href="https://marinu-m.github.io/se_project_aroundtheus/"
+              >
+                Around the US
+              </a>
+              <p className="project__list-item_elem_description">
+                <span>Tools: HTML/CSS, JavaScript, Webpack</span>
+                <span>For: BootCamp frontend project</span>
+              </p>
+            </li>
+            <li className="project__list-item">
+              <a
+                className="project__list-item_elem_link"
+                href="https://marinu-m.github.io/se_project_coffeeshop/"
+              >
+                Coffee Shop
+              </a>
+              <p className="project__list-item_elem_description">
+                <span>Tools: HTML/CSS</span>
+                <span>For: BootCamp static website project</span>
+              </p>
+            </li>
           </ul>
         </div>
       </div>
