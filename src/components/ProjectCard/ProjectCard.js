a1d7316bbf8abc684377db88d774a04439123e51
Marin M-U <marin.umegane.m@gmail.com>
2024-02-26 21:18:23 -0500

    add images

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,14 +3,17 @@ import "./project-card.css";
 function ProjectCard({ item, onSelectedCard }) {
   console.log(item);
   return (
-    <li className="project__list-item" onClick={() => onSelectedCard(item)}>
-      <span className="project__tape"></span>
+    <li
+      className="project-card__list-item"
+      onClick={() => onSelectedCard(item)}
+    >
+      <span className="project-card__tape"></span>
       <img
-        className="project__screenshot"
+        className="project-card__screenshot"
         src={item.img}
         alt="Project screenshot"
       />
-      <h3 className="project__title">{item.title}</h3>
+      <h3 className="project-card__title">{item.title}</h3>
     </li>
   );
 }
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,18 @@
 import madewithlove from "../images/madewithlove.png";
+import wtwr from "../images/wtwr.png";
+import aroundtheus from "../images/aroundtheus.png";
+import secoffee from "../images/secoffeeshop.png";
+import selibrary from "../images/selibrary.png";
+import jaillc from "../images/jaillc.png";
 
 const projectList = [
+  {
+    title: "J.A.I LLC Website",
+    url: "https://jaillctransport.com/",
+    img: jaillc,
+    tech: "HTML/CSS (responsive)",
+    for: "Freelancing project",
+  },
   {
     title: "MadewithLove",
     url: "https://github.com/MarinU-M/project-madewithlove",
@@ -11,28 +23,28 @@ const projectList = [
   {
     title: "WTWR",
     url: "https://github.com/MarinU-M/se_project_react",
-    img: madewithlove,
+    img: wtwr,
     tech: "React, CSS, Express, Node.js, MongoDB",
     for: "BootCamp fullstack project",
   },
   {
     title: "Around the US",
     url: "https://github.com/MarinU-M/se_project_aroundtheus",
-    img: madewithlove,
+    img: aroundtheus,
     tech: "HTML/CSS, JavaScript, Webpack",
     for: "BootCamp frontend project",
   },
   {
     title: "Software Engineering Coffee Shop",
     url: "https://github.com/MarinU-M/se_project_coffeeshop",
-    img: madewithlove,
+    img: secoffee,
     tech: "HTML/CSS",
     for: "BootCamp static website project",
   },
   {
     title: "Software Engineering Library",
     url: "https://github.com/MarinU-M/se_project_library",
-    src: madewithlove,
+    src: { selibrary },
     tech: "HTML/CSS",
     for: "BootCamp static website project",
   },
