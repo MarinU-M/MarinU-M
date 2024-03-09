02a3f738d4d0520af178933c91f355c3805bc852
Marin M-U <marin.umegane.m@gmail.com>
2024-03-09 13:38:00 -0500

    adjust color

--- src/components/SelfInfo/SelfInfo.js
+++ src/components/SelfInfo/SelfInfo.js
@@ -10,34 +10,44 @@ function SelfInfo() {
 
         <img src={author} className="self-info__img" alt="creator icon" />
       </div>
-      <p className="self-info__text">
-        <span>
-          Hello! I'm a junior software engineer/web developer with a background
-          in vulnerability management analysis. My passion for cybersecurity led
-          me to pursue a career in IT communication. To expand my skills and
-          gain a deeper understanding, I enrolled in&nbsp;
-        </span>
-        <a
-          href="https://tripleten.com/software-engineer/?form_position=%2Fqa-engineer%2F&gaid=2057165500.1686775753&referrer=N%2FA"
-          className="self-info__link"
-        >
-          Triple Ten's Software Engineering Bootcamp
-        </a>
-        <span>
-          , where I discovered my fascination with software engineering.
-          Creating new and exciting experiences for people through software
-          development has become my another passion. While I continue to nurture
-          my interest in cybersecurity, I'm excited to explore the world of web
-          development as my second career. My goal is to make people's online
-          experiences more secure, enjoyable, and convenient. Let's build
-          something amazing together! Feel free to explore my portfolio to
-          see&nbsp;
-        </span>
-        <Link to="/projects" className="self-info__link">
-          all of my projects
-        </Link>
-        <span>&nbsp;and accomplishments.</span>
-      </p>
+      <div className="self-info__texts">
+        <p className="self-info__text">
+          <span>
+            Hello! I'm a junior software engineer/web developer with a
+            background in vulnerability management analysis. My passion for
+            cybersecurity led me to pursue a career in IT communication. To
+            expand my skills and gain a deeper understanding, I enrolled
+            in&nbsp;
+          </span>
+          <a
+            href="https://tripleten.com/software-engineer/?form_position=%2Fqa-engineer%2F&gaid=2057165500.1686775753&referrer=N%2FA"
+            className="self-info__link"
+          >
+            Triple Ten's Software Engineering Bootcamp
+          </a>
+          <span>
+            , where I discovered my fascination with software engineering.
+            Creating new and exciting experiences for people through software
+            development has become my another passion.
+          </span>
+        </p>
+        <p className="self-info__text">
+          While I continue to nurture my interest in cybersecurity, I'm excited
+          to explore the world of web development as my second career. My goal
+          is to make people's online experiences more secure, enjoyable, and
+          convenient.
+        </p>
+        <p className="self-info__text">
+          <span>
+            Let's build something amazing together! Feel free to explore my
+            portfolio to see&nbsp;
+          </span>
+          <Link to="/projects" className="self-info__link">
+            all of my projects
+          </Link>
+          <span>&nbsp;and accomplishments.</span>
+        </p>
+      </div>
     </section>
   );
 }
